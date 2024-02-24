94050bee947a618aa362e9d30c57581e21001c4e
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-02-23 23:29:42 -0500

    adding/refactoring tmux scripts

--- tmux/a.sh
+++ tmux/a.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!bin/bash
 
-SESSION_NAME="v_def"
+SESSION_NAME="win_def"
 MAIN_WINDOW_NAME="main_win"
+SECOND_WINDOW_NAME="sec_win"
 CWD="~/projects/"
 
 # check if the session already exists, supressing output
@@ -13,14 +14,15 @@ if [ $? != 0 ]; then
   tmux new-session -s $SESSION_NAME -n $MAIN_WINDOW_NAME -d 
   # change main pane cwd 
   tmux send-keys -t $SESSION_NAME:$MAIN_WINDOW_NAME "cd $CWD" C-m 
-  # create vertical split windows
-  tmux split-window -v -l 20% -t $SESSION_NAME:$MAIN_WINDOW_NAME
-  # change cwd of the secondary pane
-  tmux send-keys -t $SESSION_NAME:$MAIN_WINDOW_NAME.1 "cd $CWD" C-m 
-  # change focus back to main pane 
-  tmux select-pane -t $SESSION_NAME:$MAIN_WINDOW_NAME.0
+  # create second window
+  tmux new-window -t $SESSION_NAME -n $SECOND_WINDOW_NAME
+  # change cwd of the secondary window
+  tmux send-keys -t $SESSION_NAME:$SECOND_WINDOW_NAME "cd $CWD" C-m 
+  # change focus back to main window 
+  tmux select-pane -t $SESSION_NAME:$MAIN_WINDOW_NAME
 
 fi 
 
 # attach to the session 
 tmux attach -t $SESSION_NAME 
+
